ff74d5c356325399f281e649ee4c00b950f4f742
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-09-01 22:13:47 +0100

    Cryptomania -V1.0

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+// Load environment variables from .env file
+const cryptoApiKey = process.env.REACT_APP_RAPIDAPI_KEY;
+const cryptoApiHost = process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST;
 
 const cryptoApiHeaders = {
-    'X-RapidAPI-Key': '3f6e155f0amsh16c2b2f69ed2054p155891jsn8a25ec5d158b',
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+    'X-RapidAPI-Key': cryptoApiKey,
+    'X-RapidAPI-Host': cryptoApiHost
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
-
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
@@ -24,5 +26,8 @@ export const cryptoApi = createApi({
   })
 });
 
-export const {useGetCryptosQuery, useGetCryptoDetailsQuery} = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
 
+
+// 'X-RapidAPI-Key': '3f6e155f0amsh16c2b2f69ed2054p155891jsn8a25ec5d158b',
+// 'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
